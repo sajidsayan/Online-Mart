5f85956e313c21be5f8cb1664e2479167de22a8a
Evil_Sensei <sajidmahmudsayan909@gmail.com>
2025-02-28 17:49:22 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,61 @@
-let i = 0
+const copyDiscountCoupon = document.getElementById('discount-coupon')
 
-function handleClick(){
-    document.getElementById('clickBtn').textContent = `Count: ${i}`;
-    i++;
+copyDiscountCoupon.addEventListener('click' , function(){
+    
+    const discountText = copyDiscountCoupon.innerText;
+    const textarea = document.createElement('textarea');
+    textarea.value = discountText;
+    document.body.appendChild(textarea);
+
+
+    textarea.select();
+    textarea.setSelectionRange(0 , 99999);
+    document.execCommand('copy')
+
+    document.body.removeChild(textarea);
+    copyDiscountCoupon.innerText = "Copied!";
+
+    setTimeout(() => {
+        copyDiscountCoupon.textContent = discountText;
+    },1500);
+})
+
+
+
+const couponButton = document.getElementById('coupon-button')
+const purchaseButton = document.getElementById('purchase-button')
+const totalPriceElement = document.getElementById('total-price')
+const totalDiscount = document.getElementById('total-discount')
+const priceAfterDiscount = document.getElementById('price-after-discount')
+const cartItem = document.getElementById('cart-item')
+
+let itemList = [];
+let totalPrice = 0;
+let discount = 0;
+
+couponButton.disabled = true;
+purchaseButton.disabled = true;
+
+function additem(item, price){
+    itemList.push({ name: item, price: price})
+    totalPrice += price
+    updatePrice();
 }
 
-function handleMouseOver(){
-    document.getElementById('hoverBtn').classList.replace('bg-green-500', 'bg-red-500')
-    document.getElementById('hoverBtn').innerText = 'I am on Focus';
+function removeitem(index){
+    if (index >= 0 && index < itemList.length){
+        totalPrice -= itemList[index].price;
+        itemList.splice(index, 1);
+        updatePrice();
+    }
 }
 
-function handleMouseout(){
-    document.getElementById('hoverBtn').classList.replace('bg-red-500', 'bg-green-500')
-    document.getElementById('hoverBtn').innerText = 'Hover Me';
+function updatePrice(){
+    totalPriceElement.innerText = totalPrice.toFixed(2);
+    discount = 0;
+    totalPriceElement.innerText = discount.toFixed(2);
+    priceAfterDiscount.innerText = (totalPrice - discount).toFixed(2)
+    purchaseButton.disabled = totalPrice === 0;
+    couponButton.disabled = totalPrice < 100000
+    renderList()
 }
